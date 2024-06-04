b3e04e29691be8acf3334b18cb5ae49eeb5518de
0x xj jda <ranahari@gmail.com>
2024-06-05 00:20:05 +0800

    updated+deleted scripts

--- twitter-api-v2-search-tweets.js
+++ twitter-api-v2-search-tweets.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 const { TwitterApi } = require('twitter-api-v2');
 
 // Load Twitter API keys and tokens from .env file
-const client = new TwitterApi(process.env.BEARER_TOKEN);
-// const client = new TwitterApi({
-//   appKey: process.env.CONSUMER_KEY,
-//   appSecret: process.env.CONSUMER_SECRET,
-//   accessToken: process.env.ACCESS_TOKEN,
-//   accessSecret: process.env.ACCESS_TOKEN_SECRET,
-// });
+// const client = new TwitterApi(process.env.BEARER_TOKEN);
+const client = new TwitterApi({
+  appKey: process.env.CONSUMER_KEY,
+  appSecret: process.env.CONSUMER_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessSecret: process.env.ACCESS_TOKEN_SECRET,
+});
 
 // Function to search and read tweets
 const searchTweetsFromUser = async (query, username) => {
