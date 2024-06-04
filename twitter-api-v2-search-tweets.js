a418d034b6202d217eb6cc0545bb34ca66eddde3
harry rana <>
2024-06-05 01:12:56 +0800

    updated: search tweets

--- twitter-api-v2-search-tweets.js
+++ twitter-api-v2-search-tweets.js
@@ -4,23 +4,26 @@ const { TwitterApi } = require('twitter-api-v2');
 
 // Load Twitter API keys and tokens from .env file
 // const client = new TwitterApi(process.env.BEARER_TOKEN);
-const client = new TwitterApi({
-  appKey: process.env.CONSUMER_KEY,
-  appSecret: process.env.CONSUMER_SECRET,
-  accessToken: process.env.ACCESS_TOKEN,
-  accessSecret: process.env.ACCESS_TOKEN_SECRET,
-});
+
 
 // Function to search and read tweets
 const searchTweetsFromUser = async (query, username) => {
     try {
-        const tweets = await client.v2.search(query, {
-            'tweet.fields': 'author_id,created_at',
-            expansions: 'author_id',
-            max_results: 100,
-            query: `from:${username} "${query}"`,
+        const client = await new TwitterApi({
+            appKey: process.env.CONSUMER_KEY,
+            appSecret: process.env.CONSUMER_SECRET,
+            accessToken: process.env.ACCESS_TOKEN,
+            accessSecret: process.env.ACCESS_TOKEN_SECRET,
         });
-        console.log('tweets:', tweets.data);
+
+        const tweets = await client.v2.search(`from:${username} "${query}"`, {
+            max_results: 100
+        });
+        // Consume every possible tweet of jsTweets (until rate limit is hit)
+        for await (const tweet of tweets) {
+            // console.log(tweet);
+            console.log('tweet:', tweet);
+        }
     } catch (error) {
         console.log('tweets catch:', error);
     }
