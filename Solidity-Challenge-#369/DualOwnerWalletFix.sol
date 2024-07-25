72b6159600ce629193a619ce2e20796a869b4572
harry rana <ranahari@gmail.com>
2024-07-26 00:20:51 +0800

    prettier:369

--- Solidity-Challenge-#369/DualOwnerWalletFix.sol
+++ Solidity-Challenge-#369/DualOwnerWalletFix.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
 
 contract DualOwnerWalletFix {
     using ECDSA for bytes32;
@@ -14,34 +14,19 @@ contract DualOwnerWalletFix {
 
     function contribute() external payable {}
 
-    function sendFunds(
-        address recipient,
-        uint amount,
-        bytes[2] memory signatures
-    ) external {
+    function sendFunds(address recipient, uint amount, bytes[2] memory signatures) external {
         bytes32 transactionHash = calculateTxHash(recipient, amount);
-        require(
-            verifySignatures(signatures, transactionHash),
-            "Invalid signature"
-        );
+        require(verifySignatures(signatures, transactionHash), 'Invalid signature');
 
-        (bool success, ) = recipient.call{value: amount}("");
-        require(success, "Failed to send Ether");
+        (bool success, ) = recipient.call{ value: amount }('');
+        require(success, 'Failed to send Ether');
     }
 
-    function calculateTxHash(address recipient, uint amount)
-        public
-        view
-        returns (bytes32)
-    {
+    function calculateTxHash(address recipient, uint amount) public view returns (bytes32) {
         return keccak256(abi.encodePacked(recipient, amount));
     }
 
-    function verifySignatures(bytes[2] memory signatures, bytes32 transactionHash)
-        private
-        view
-        returns (bool)
-    {
+    function verifySignatures(bytes[2] memory signatures, bytes32 transactionHash) private view returns (bool) {
         // Use the ECDSA library's toEthSignedMessageHash function
         bytes32 ethSignedHash = transactionHash.toEthSignedMessageHash();
         for (uint i = 0; i < signatures.length; i++) {
